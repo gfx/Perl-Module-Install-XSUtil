3da1eb53706762bdced2e03face0d8005c3b2a1e
gfx <gfuji@cpan.org>
2009-08-12 11:36:51 +0900

    version++, changelogging

--- lib/Module/Install/XSUtil.pm
+++ lib/Module/Install/XSUtil.pm
@@ -2,7 +2,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.07';
+$VERSION = '0.08';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -443,7 +443,7 @@ Module::Install::XSUtil - Utility functions for XS modules
 
 =head1 VERSION
 
-This document describes Module::Install::XSUtil version 0.07.
+This document describes Module::Install::XSUtil version 0.08.
 
 =head1 SYNOPSIS
 
