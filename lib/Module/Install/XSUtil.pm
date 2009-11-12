7e919e7a54553479002193a250505cc73d8fabc6
gfx <gfuji@cpan.org>
2009-11-12 13:31:07 +0900

    Checking in changes prior to tagging of version 0.17.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 3085dc2..a7fc5a1 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Module::Install::XSUtil
    
    +0.17 Thu Nov 12 13:30:47 2009
    +    - Fix cc_warnings() for old gcc
    +    - Add cc_available() using ExtUtils::CBuuilder
    +
     0.16 Mon Nov  2 11:55:46 2009
     	- Fix a typo in cc_warnings()

--- lib/Module/Install/XSUtil.pm
+++ lib/Module/Install/XSUtil.pm
@@ -2,7 +2,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.16';
+$VERSION = '0.17';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -521,7 +521,7 @@ Module::Install::XSUtil - Utility functions for XS modules
 
 =head1 VERSION
 
-This document describes Module::Install::XSUtil version 0.16.
+This document describes Module::Install::XSUtil version 0.17.
 
 =head1 SYNOPSIS
 
