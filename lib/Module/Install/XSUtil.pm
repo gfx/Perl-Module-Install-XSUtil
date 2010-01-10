16cc14c6313d99fc4c9358add4100037c8cffcc9
gfx <gfuji@cpan.org>
2010-01-10 15:23:02 +0900

    Checking in changes prior to tagging of version 0.21.  Changelog diff is:
    diff --git a/Changes b/Changes
    index b9b24f9..fd22eaa 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Module::Install::XSUtil
    
    +0.21 Sun Jan 10 15:22:20 2010
    +    - Enable -Wc++-compat on gcc >= 4.1
    +
     0.20 Sat Jan  9 16:31:53 2010
         - Change cc_warnings()
             - Add -Wc++-compat on gcc 4

--- lib/Module/Install/XSUtil.pm
+++ lib/Module/Install/XSUtil.pm
@@ -2,7 +2,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.20';
+$VERSION = '0.21';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -536,7 +536,7 @@ Module::Install::XSUtil - Utility functions for XS modules
 
 =head1 VERSION
 
-This document describes Module::Install::XSUtil version 0.20.
+This document describes Module::Install::XSUtil version 0.21.
 
 =head1 SYNOPSIS
 
