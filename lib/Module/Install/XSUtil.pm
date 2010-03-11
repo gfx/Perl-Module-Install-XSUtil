1017357f1a707560276dc529a3a8a63d6d92bd95
gfx <gfuji@cpan.org>
2010-03-11 19:02:54 +0900

    Checking in changes prior to tagging of version 0.22.  Changelog diff is:
    diff --git a/Changes b/Changes
    index fd22eaa..a89d963 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Module::Install::XSUtil
    
    +0.22 Thu Mar 11 19:02:18 2010
    +    - Add Devel::PPPort to build_requires if needed
    +
     0.21 Sun Jan 10 15:22:20 2010
         - Enable -Wc++-compat on gcc >= 4.1

--- lib/Module/Install/XSUtil.pm
+++ lib/Module/Install/XSUtil.pm
@@ -2,7 +2,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.21';
+$VERSION = '0.22';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -537,7 +537,7 @@ Module::Install::XSUtil - Utility functions for XS modules
 
 =head1 VERSION
 
-This document describes Module::Install::XSUtil version 0.21.
+This document describes Module::Install::XSUtil version 0.22.
 
 =head1 SYNOPSIS
 
