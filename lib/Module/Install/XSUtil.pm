37f4714f836c7c74f64af12e6ef264359dada069
gfx <gfuji@cpan.org>
2010-07-13 19:44:53 +0900

    Checking in changes prior to tagging of version 0.25.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c102d34..a3d8e3e 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Module::Install::XSUtil
    
    +0.25 Tue Jul 13 19:43:34 2010
    +    - Add requires_c99() to assert that the compiler supports C99
    +    - Fix Win32 problems, hopefully
    +
     0.24 Sun May 16 13:24:58 2010
         - Compliant with Module::Install 0.97

--- lib/Module/Install/XSUtil.pm
+++ lib/Module/Install/XSUtil.pm
@@ -2,7 +2,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.24';
+$VERSION = '0.25';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -627,7 +627,7 @@ Module::Install::XSUtil - Utility functions for XS modules
 
 =head1 VERSION
 
-This document describes Module::Install::XSUtil version 0.24.
+This document describes Module::Install::XSUtil version 0.25.
 
 =head1 SYNOPSIS
 
