537ec36ebc2ccbd25cd8d66f1b478a44f61b2a0e
Fuji, Goro <gfuji@cpan.org>
2011-05-16 23:38:24 +0900

    Checking in changes prior to tagging of version 0.37.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3985fe2..2becf55 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Module::Install::XSUtil
    
    +0.37 2011-05-16 23:33:49
    +    - Makefile.PL -g implices "-g -ggdb -g3" for gcc (EXPERIMENTAL)
    +    - PERL_ONLY=1 means --pp for modules with XS/PP dual backends
    +
     0.36 Thu Oct  7 16:49:04 2010
         - Resolve RT #61660 (SREZIC): Support for ccache missing
         - Resolve RT #61770 (bokutin): A typo in SYNOPSIS

--- lib/Module/Install/XSUtil.pm
+++ lib/Module/Install/XSUtil.pm
@@ -2,7 +2,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.36';
+$VERSION = '0.37';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -780,7 +780,7 @@ Module::Install::XSUtil - Utility functions for XS modules
 
 =head1 VERSION
 
-This document describes Module::Install::XSUtil version 0.36.
+This document describes Module::Install::XSUtil version 0.37.
 
 =head1 SYNOPSIS
 
