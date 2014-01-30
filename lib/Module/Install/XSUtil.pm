16cf9e7d9dd0699b808e98a5629102043deef677
Fuji, Goro (gfx) <gfuji@cpan.org>
2014-01-31 07:14:50 +0900

    Checking in changes prior to tagging of version 0.45.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 445ad3c..d3323b1 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,7 +1,10 @@
     Revision history for Perl extension Module::Install::XSUtil
    
    -0.44 2013-04-24 11:30:07
    +0.45 2014-01-31 07:13:52+0900
    +    - Requires ExtUtils::ParseXS 3.18 because of 2.21 issues
    +      (https://github.com/xslate/p5-Text-Xslate/issues/104)
    
    +0.44 2013-04-24 11:30:07
         - Support PUREPERL_ONLY=1
    
     0.43 2012-06-25 09:23:04

--- lib/Module/Install/XSUtil.pm
+++ lib/Module/Install/XSUtil.pm
@@ -2,7 +2,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.44';
+$VERSION = '0.45';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -823,7 +823,7 @@ Module::Install::XSUtil - Utility functions for XS modules
 
 =head1 VERSION
 
-This document describes Module::Install::XSUtil version 0.44.
+This document describes Module::Install::XSUtil version 0.45.
 
 =head1 SYNOPSIS
 
